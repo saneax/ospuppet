6f487ec74d983bcb743024d1057cec10bebc4505
Sanjay Upadhyay <saneax@gmail.com>
2015-04-13 09:43:27 +0000

    directory changes

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,26 +1,2 @@
-#node "ct1" {
-
-#  include system
-
-#  class {
-#    'mysql::server':
-#  }
-
-
-  #class { 'keystone::db::mysql':
-  #  password      => 'super_secret_db_password',
-  #  allowed_hosts => '%',
-  #}
-
-  #class { 'keystone':
-  #	verbose        => True,
-  #	catalog_type   => 'sql',
-  #	admin_token    => 'random_uuid',
-  #	sql_connection => 'mysql://keystone:super_secret_db_password@ct1.os.loc/keystone',
-  #}
-
-#  }
-
-class { "ntp": }
-
-
+class { "::scontrol": }
+class { "::scontrol::accounts": }
